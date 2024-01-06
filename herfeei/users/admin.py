0fefac3e6238385e6993e833602355abc36c648f
kouhkan <amirkouhkan1@gmail.com>
2024-01-06 17:57:03 +0330

    refactor: is_superuser instead of is_admin

--- herfeei/users/admin.py
+++ herfeei/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from herfeei.users.models import BaseUser, Profile
+
+
+@admin.register(BaseUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "role", "is_superuser")
+    filter = ("role",)
+    search_fields = ("username", "email")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "full_name")
+    search_fields = ("user",)

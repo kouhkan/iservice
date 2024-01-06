ba97f3a85eaca5e856d03a5ad8c8a4a08dabee8c
kouhkan <amirkouhkan1@gmail.com>
2024-01-06 20:24:15 +0330

    feat: users selector for auth

---
+++ herfeei/authentication/selectors/users.py
@@ -0,0 +1,11 @@
+from herfeei.users.models import BaseUser
+
+
+def get_user_by_username(*, username: str) -> BaseUser:
+    return BaseUser.objects.filter(username=username).first()
+
+
+def get_user_by_email(*, email: str) -> BaseUser:
+    return BaseUser.objects.filter(email=email).first()
+
+

ba804ac9010914f63c6cc6cd48ade2fecbee0730
kouhkan <amirkouhkan1@gmail.com>
2024-01-06 17:30:15 +0330

    refactor: user model

--- herfeei/users/models.py
+++ herfeei/users/models.py
@@ -4,15 +4,15 @@ from herfeei.common.models import BaseModel
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager as BUM
 from django.contrib.auth.models import PermissionsMixin
-
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
+    def create_user(self, username, email=None, password=None):
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError(_("Users must have an email address"))
 
-        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+        user = self.model(username=username, email=self.normalize_email(email.lower()))
 
         if password is not None:
             user.set_password(password)
@@ -24,51 +24,44 @@ class BaseUserManager(BUM):
 
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, username, email, password=None):
         user = self.create_user(
+            username=username,
             email=email,
-            is_active=True,
-            is_admin=True,
             password=password,
         )
 
-        user.is_superuser = True
+        user.role = BaseUser.UserRole.ADMIN
         user.save(using=self._db)
 
         return user
 
 
 class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+    class UserRole(models.TextChoices):
+        ADMIN = "ADMIN"
+        USER = "USER"
+        EXPERT = "EXPERT"
 
-    email = models.EmailField(verbose_name = "email address",
-                              unique=True)
-
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    username = models.CharField(_("Phone number"), max_length=14, unique=True, db_index=True)
+    email = models.EmailField(_("email address"), unique=True, db_index=True, null=True, blank=True)
+    role = models.CharField(max_length=9, choices=UserRole.choices, default=UserRole.USER)
 
     objects = BaseUserManager()
 
-    USERNAME_FIELD = "email"
-
     def __str__(self):
-        return self.email
+        return self.username
 
     def is_staff(self):
-        return self.is_admin
+        return self.role == self.UserRole.ADMIN
+
+    def is_admin(self):
+        return self.role == self.UserRole.ADMIN
 
 
 class Profile(models.Model):
     user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
-    posts_count = models.PositiveIntegerField(default=0)
-    subscriber_count = models.PositiveIntegerField(default=0)
-    subscription_count = models.PositiveIntegerField(default=0)
     bio = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user} >> {self.bio}"
-
-
-
-
-
-

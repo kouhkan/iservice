8ef65335d1d30224a13d7481d50c7f837cd980f9
kouhkan <amirkouhkan1@gmail.com>
2024-01-06 18:02:13 +0330

    refactor: rename, change query to get profile

---
+++ herfeei/users/services/users.py
@@ -0,0 +1,19 @@
+from django.db import transaction
+from herfeei.users.models import BaseUser, Profile
+
+
+def create_profile(*, user: BaseUser, ) -> Profile:
+    return Profile.objects.create(user=user)
+
+
+def create_user(*, username: str) -> BaseUser:
+    return BaseUser.objects.get_or_create(
+        username=username, defaults={"username": username}
+    )
+
+
+@transaction.atomic
+def register(*, username: str) -> BaseUser:
+    user = create_user(username=username)
+    create_profile(user=user)
+    return user

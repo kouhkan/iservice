fc4970d60e772409027ef5b344df26ea6042b096
kouhkan <amirkouhkan1@gmail.com>
2024-01-06 18:22:21 +0330

    feat: add users urls

--- herfeei/api/urls.py
+++ herfeei/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    # path('blog/', include(('herfeei.blog.urls', 'blog')))
+    path("users/", include(("herfeei.users.urls", "users"), namespace="users")),
 ]

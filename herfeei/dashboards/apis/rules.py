eb58cdb3e57cbe3503b23ea5bc9ba17c2f32e05f
kouhkan <amirkouhkan1@gmail.com>
2024-01-07 20:35:10 +0330

    feat: add rules view

---
+++ herfeei/dashboards/apis/rules.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from herfeei.dashboards.models import Rule
+from herfeei.dashboards.selectors.rules import get_rules
+
+
+class GetRulesView(APIView):
+    class OutputRulesSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Rule
+            fields = (
+                "id", "title", "subtitle",
+                "details", "created_at", "updated_at",
+                "slug"
+            )
+
+    def get(self, request):
+        return Response(self.OutputRulesSerializer(get_rules(), many=True).data)

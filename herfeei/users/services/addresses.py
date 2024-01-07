2af041e0858b98e6c117195df635956f6ce1075e
kouhkan <amirkouhkan1@gmail.com>
2024-01-07 18:56:11 +0330

    feat: create user services

---
+++ herfeei/users/services/addresses.py
@@ -0,0 +1,40 @@
+from django.db import transaction
+from django.utils.text import slugify
+
+from herfeei.users.models import BaseUser, Address
+
+
+@transaction.atomic
+def create_address(*, user: BaseUser, title: str, details: str, default: bool = False, phone: str = None,
+                   lat: float = None, long: float = None) -> Address:
+    return Address.objects.create(user=user, title=title, slug=slugify(title), details=details, default=default,
+                                  phone=phone, lat=lat, long=long)
+
+
+def update_address(*, user: BaseUser, address_id: int, **kwargs) -> bool:
+    if not (address := Address.objects.filter(user=user, id=address_id).first()):
+        return False
+
+    for key, value in kwargs.items():
+        address[key] = value
+    address.save()
+
+    return True
+
+
+def delete_address(*, user: BaseUser, address_id: int) -> bool:
+    if not (address := Address.objects.filter(user=user, id=address_id).first()):
+        return False
+    address.delete()
+
+    return True
+
+
+def change_default_address(*, user: BaseUser, address_id: int) -> bool:
+    if not (address := Address.objects.filter(user=user, id=address_id).first()):
+        return False
+
+    Address.objects.filter(user=user).update(default=False)
+    address.default = True
+    address.save()
+    return True

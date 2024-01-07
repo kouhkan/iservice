aaab178a8c89518f61882b254bd48e38a1ae4b8d
kouhkan <amirkouhkan1@gmail.com>
2024-01-07 15:30:34 +0330

    feat: update avatar and profile views

--- herfeei/dashboards/apis/users.py
+++ herfeei/dashboards/apis/users.py
@@ -9,17 +9,17 @@ from herfeei.users.models import Profile
 
 class UpdateUserProfileView(ApiAuthMixin, APIView):
     class InputUpdateProfileSerializer(serializers.Serializer):
-        full_name = serializers.CharField(max_length=64, allow_null=True, allow_blank=True)
-        date_of_birth = serializers.DateField(allow_null=True, allow_blank=True)
-        gender = serializers.CharField(max_length=6, allow_null=True, allow_blank=True)
-        city = serializers.CharField(max_length=128, allow_null=True, allow_blank=True)
-        email = serializers.EmailField(max_length=128, allow_null=True, allow_blank=True)
-        username = serializers.CharField(min_length=10, max_length=10, allow_null=True, allow_blank=True)
+        full_name = serializers.CharField(max_length=64, allow_null=True, allow_blank=True, required=False)
+        date_of_birth = serializers.DateField(allow_null=True, required=False)
+        gender = serializers.CharField(max_length=6, allow_null=True, allow_blank=True, required=False)
+        city = serializers.CharField(max_length=128, allow_null=True, allow_blank=True, required=False)
+        email = serializers.EmailField(max_length=128, allow_null=True, allow_blank=True, required=False)
+        username = serializers.CharField(min_length=10, max_length=10, allow_null=True, allow_blank=True, required=False)
 
     class OutputUpdateProfileSerializer(serializers.ModelSerializer):
         class Meta:
             model = Profile
-            fields = ("full_name", "date_of_birth", "gender", "city", "created_at")
+            fields = ("full_name", "date_of_birth", "gender", "city", "avatar")
 
     @extend_schema(request=InputUpdateProfileSerializer, responses=OutputUpdateProfileSerializer)
     def patch(self, request):
@@ -35,6 +35,37 @@ class UpdateUserProfileView(ApiAuthMixin, APIView):
             request.user.email = serializer.validated_data.get("email", request.user.email)
             request.user.username = serializer.validated_data.get("username", request.user.username)
             request.user.save()
-            return Response("user's data updated", status=status.HTTP_200_OK)
+            return Response(self.OutputUpdateProfileSerializer(request.user.profile).data, status=status.HTTP_200_OK)
         except Exception as ex:
             return Response(f"{ex}", status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateUserAvatarView(ApiAuthMixin, APIView):
+    class InputUserAvatarSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Profile
+            fields = ("avatar",)
+
+    class OutputUserAvatarSerializer(serializers.ModelSerializer):
+        avatar_url = serializers.SerializerMethodField("get_avatar_url")
+
+        class Meta:
+            model = Profile
+            fields = ("avatar_url", "full_name", "gender", "date_of_birth")
+
+        def get_avatar_url(self, obj):
+            if obj.avatar:
+                request = self.context.get("request")
+
+                if request:
+                    return request.build_absolute_uri(obj.avatar.url)
+            return None
+
+    def patch(self, request):
+        serializer = self.InputUserAvatarSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        request.user.profile.avatar = serializer.validated_data.get("avatar", request.user.profile.avatar)
+        request.user.profile.save()
+
+        return Response(self.OutputUserAvatarSerializer(request.user.profile, context={"request": request}).data)

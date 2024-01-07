1323469910c016260236d63f3d7db84f149c4386
kouhkan <amirkouhkan1@gmail.com>
2024-01-07 20:34:44 +0330

    feat: create Rules model

--- herfeei/dashboards/models.py
+++ herfeei/dashboards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from herfeei.common.models import BaseModel
+
+
+class Rule(BaseModel):
+    title = models.CharField(max_length=64)
+    subtitle = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=32, unique=True, allow_unicode=True, db_index=True)
+    details = models.TextField()
+    enable = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+

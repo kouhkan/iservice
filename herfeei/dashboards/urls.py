e9475b743b1b690be737831875989e6b7ba27997
kouhkan <amirkouhkan1@gmail.com>
2024-01-07 20:15:55 +0330

    feat: add change default address to dashboard urls

--- herfeei/dashboards/urls.py
+++ herfeei/dashboards/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenBlacklistView
 
 from herfeei.dashboards.apis.users import UpdateUserAvatarView, UpdateUserProfileView
 from herfeei.users.apis.addresses import CreateUserAddressView, GetUserAddressesListView, GetUserAddressView, \
-    UpdateUserAddressView
+    UpdateUserAddressView, DeleteUserAddressView, ChangeDefaultUserAddressView
 
 urlpatterns = [
     path("", include([
@@ -17,6 +17,8 @@ urlpatterns = [
             path("<int:id>/", GetUserAddressView.as_view(), name="get-user-address"),
             path("list/", GetUserAddressesListView.as_view(), name="get-user-addresses"),
             path("update/<int:id>/", UpdateUserAddressView.as_view(), name="update-user-addresses"),
+            path("delete/<int:id>/", DeleteUserAddressView.as_view(), name="delete-user-addresses"),
+            path("default/<int:id>/", ChangeDefaultUserAddressView.as_view(), name="change-default-user-addresses"),
         ]))
     ]))
 ]

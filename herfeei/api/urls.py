fdc746591aaf78b6da518c31cdb2e368a4b94b05
kouhkan <amirkouhkan1@gmail.com>
2024-01-07 15:30:06 +0330

    feat: add dashboards

--- herfeei/api/urls.py
+++ herfeei/api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("users/", include(("herfeei.users.urls", "users"), namespace="users")),
     path("auth/", include(("herfeei.authentication.urls", "users"), namespace="auth")),
+    path("dashboards/", include(("herfeei.dashboards.urls", "dashboards"), namespace="dashboards")),
 ]

9170d60a895352e40a06024410817f09cbe8a598
kouhkan <amirkouhkan1@gmail.com>
2024-01-08 16:21:43 +0330

    feat: create faq and faq category models

--- herfeei/dashboards/models.py
+++ herfeei/dashboards/models.py
@@ -8,9 +8,32 @@ class Rule(BaseModel):
     subtitle = models.CharField(max_length=128)
     slug = models.SlugField(max_length=32, unique=True, allow_unicode=True, db_index=True)
     details = models.TextField()
-    enable = models.BooleanField(default=True)
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
 
 
+class FaqCategory(BaseModel):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=32, unique=True, db_index=True, allow_unicode=True)
+    icon = models.CharField(max_length=512, null=True, blank=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "FAQ Category"
+        verbose_name_plural = "FAQ Categories"
+
+    def __str__(self):
+        return self.title
+
+
+class Faq(BaseModel):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=32, unique=True, db_index=True, allow_unicode=True)
+    details = models.TextField()
+    category = models.ForeignKey(FaqCategory, on_delete=models.CASCADE, related_name="faq")
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title

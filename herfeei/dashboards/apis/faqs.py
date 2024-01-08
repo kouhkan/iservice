2f6fb1eb92fa69949d9072f48dc33058d38f1bbf
kouhkan <amirkouhkan1@gmail.com>
2024-01-08 16:51:02 +0330

    feat: get list of faq categories

--- herfeei/dashboards/apis/faqs.py
+++ herfeei/dashboards/apis/faqs.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from herfeei.dashboards.models import Faq
-from herfeei.dashboards.selectors.faqs import get_faqs
+from herfeei.dashboards.models import Faq, FaqCategory
+from herfeei.dashboards.selectors.faqs import get_faqs, get_faq_categories
 
 
 class FaqView(APIView):
@@ -19,3 +19,14 @@ class FaqView(APIView):
     @extend_schema(responses=OutputFaqSerializer)
     def get(self, request, slug):
         return Response(self.OutputFaqSerializer(get_faqs(slug_category=slug), many=True).data)
+
+
+class FaqCategoriesView(APIView):
+    class OutputFaqCategoriesSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = FaqCategory
+            fields = ("id", "title", "slug", "icon", "created_at")
+
+    @extend_schema(responses=OutputFaqCategoriesSerializer)
+    def get(self, request):
+        return Response(self.OutputFaqCategoriesSerializer(get_faq_categories(), many=True).data)

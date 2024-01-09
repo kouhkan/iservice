07a92a4c629679d1cb6b614b1aac667edce47f61
kouhkan <amirkouhkan1@gmail.com>
2024-01-09 23:25:48 +0330

    refactor: rename function and integrate faraz sms

--- herfeei/authentication/tasks.py
+++ herfeei/authentication/tasks.py
@@ -1,6 +1,9 @@
 from celery import shared_task
 
+from herfeei.authentication.utils.sms import send_sms
+
 
 @shared_task
-def send_sms(username: str, token: int) -> None:
+def send_sms_task(username: str, token: int) -> None:
     print(f"{username} -> {token}")
+    send_sms(receiver=username, token=token)

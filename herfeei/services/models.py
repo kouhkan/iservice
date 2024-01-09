e80ebbbf9d99785a8667d2282af6ba6d1d99eb87
kouhkan <amirkouhkan1@gmail.com>
2024-01-09 17:09:21 +0330

    feat: service category model

--- herfeei/services/models.py
+++ herfeei/services/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from treebeard.mp_tree import MP_Node
 
 from herfeei.common.models import BaseModel
 
@@ -21,3 +22,16 @@ class City(BaseModel):
 
     class Meta:
         verbose_name_plural = "Cities"
+
+
+class ServiceCategory(MP_Node, BaseModel):
+    title = models.CharField(max_length=128, db_index=True)
+    slug = models.SlugField(max_length=128, db_index=True, unique=True, allow_unicode=True)
+    description = models.CharField(max_length=512, null=True, blank=True)
+    is_public = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Service Categories"
+
+    def __str__(self):
+        return self.title

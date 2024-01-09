25b943c8db5f46c41429c6f92259f265c8bc3d9a
kouhkan <amirkouhkan1@gmail.com>
2024-01-09 19:32:44 +0330

    feat: get services category view

--- herfeei/services/apis/services.py
+++ herfeei/services/apis/services.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from herfeei.services.models import ServiceCategory
-from herfeei.services.selectors.services import get_service_categories
+from herfeei.services.selectors.services import get_service_categories, get_children_service_category
 
 
 class GetServiceCategoryView(APIView):
@@ -15,3 +15,18 @@ class GetServiceCategoryView(APIView):
 
     def get(self, request):
         return Response(self.OutputGetServiceCategorySerializer(get_service_categories(), many=True).data)
+
+
+class GetChildrenServiceCategoryView(APIView):
+    class OutputGetChildrenServiceCategorySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = ServiceCategory
+            # fields = ("title", "slug", "description", "created_at")
+            fields = "__all__"
+
+    def get(self, request, slug):
+        if not (data := get_children_service_category(slug=slug)):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            self.OutputGetChildrenServiceCategorySerializer(data, many=True).data
+        )
--- herfeei/services/urls.py
+++ herfeei/services/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
 from herfeei.services.apis.locations import GetProvincesView, GetCitiesView
-from herfeei.services.apis.services import GetServiceCategoryView
+from herfeei.services.apis.services import GetServiceCategoryView, GetChildrenServiceCategoryView
 
 urlpatterns = [
     path("categories/", GetServiceCategoryView.as_view(), name="get-service-categories"),
+    path("categories/<str:slug>/", GetChildrenServiceCategoryView.as_view(), name="get-children-service-categories"),
 
     path("locations/", include([
         path("", GetProvincesView.as_view(), name="get-provinces"),

e3439257f5eaaa6692d0dfb63ab36def0cc911c5
kouhkan <amirkouhkan1@gmail.com>
2024-01-09 17:09:58 +0330

    feat: City, Province and Service Category Admin

--- herfeei/services/admin.py
+++ herfeei/services/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-# Register your models here.
+from herfeei.services.models import City, Province, ServiceCategory
+
+
+class CityTabularAdmin(admin.TabularInline):
+    model = City
+    extra = 3
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_at")
+    search_fields = ("name",)
+    list_per_page = 25
+    inlines = (CityTabularAdmin,)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(TreeAdmin):
+    form = movenodeform_factory(ServiceCategory)
+    prepopulated_fields = {"slug": ("title",)}

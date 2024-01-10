c1ae21b7ed8fa391dcb2c3aa28ce752d982ded43
kouhkan <amirkouhkan1@gmail.com>
2024-01-10 17:53:19 +0330

    feat: notification and user notification admin panel

--- herfeei/notifications/admin.py
+++ herfeei/notifications/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from herfeei.notifications.models import Notification, UserNotification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "level", "created_at")
+    list_filter = ("level",)
+    list_per_page = 25
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(UserNotification)
+class UserNotificationAdmin(admin.ModelAdmin):
+    list_display = ("user", "notification", "status", "created_at")
+    search_fields = ("user", "notification")
+    list_per_page = 25

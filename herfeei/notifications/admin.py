05067061346ab22a84ec64307bc312982f594fa1
kouhkan <amirkouhkan1@gmail.com>
2024-01-11 18:15:44 +0330

    feat: notification admins

--- herfeei/notifications/admin.py
+++ herfeei/notifications/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-from herfeei.notifications.models import Notification, UserNotification
+from herfeei.notifications.models import BaseNotification, UserNotification, NotificationCategory, NotificationOption
 
 
-@admin.register(Notification)
+@admin.register(NotificationCategory)
+class NotificationCategoryAdmin(TreeAdmin):
+    form = movenodeform_factory(NotificationCategory)
+    list_display = ("title", "slug", "created_at")
+    search_fields = ("title",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_per_page = 25
+
+
+@admin.register(BaseNotification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "level", "created_at")
     list_filter = ("level",)
     list_per_page = 25
     prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(NotificationOption)
+class NotificationOptionTabularAdmin(admin.ModelAdmin):
+    list_display = ("item", "value")
+    list_per_page = 25
 
 
 @admin.register(UserNotification)

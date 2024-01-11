598140b1dde0f51b738940b0569fb5af58e5f14c
kouhkan <amirkouhkan1@gmail.com>
2024-01-11 21:10:26 +0330

    feat: add read notification url

--- herfeei/notifications/urls.py
+++ herfeei/notifications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from herfeei.notifications.apis.notifications import GetUserNotificationsView
+from herfeei.notifications.apis.notifications import GetUserNotificationsView, ReadUserNotificationView
 
 urlpatterns = [
     path("", GetUserNotificationsView.as_view(), name="get-user-notifications"),
+    path("read/<int:id>/", ReadUserNotificationView.as_view(), name="read-user-notification"),
 ]

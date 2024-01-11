d2d63fdf4a92ac3627cd391f064c88343a41bb2a
kouhkan <amirkouhkan1@gmail.com>
2024-01-11 21:10:05 +0330

    refactor: remove unused functions and rename read function name

--- herfeei/notifications/services/notifications.py
+++ herfeei/notifications/services/notifications.py
@@ -1,26 +1,8 @@
-from django.utils.text import slugify
-
-from herfeei.notifications.models import UserNotification, BaseNotification
+from herfeei.notifications.models import UserNotification
 from herfeei.users.models import BaseUser
 
 
-def create_base_notification(
-        *,
-        title: str,
-        description: str,
-        level: BaseNotification.NotificationLevel = BaseNotification.NotificationLevel.INFO
-) -> BaseNotification:
-    return BaseNotification.objects.create(title=title, slug=slugify(title), description=description, level=level)
-
-
-def create_welcome_notification(*, user: BaseUser) -> UserNotification:
-    title = "خوش آمدید"
-    description = f"کاربر {user} به اپلیکیشن حرفه‌ای خوش آمدید"
-    notification = create_base_notification(title=title, description=description)
-    return UserNotification.objects.create(user=user, notification=notification)
-
-
-def read_notification(*, user: BaseUser, notification_id: int) -> UserNotification | bool:
+def change_to_read_notification(*, user: BaseUser, notification_id: int) -> UserNotification | bool:
     if not (notification := UserNotification.objects.filter(
             id=notification_id, user=user,
             status=UserNotification.NotificationStatus.UNREAD).first()

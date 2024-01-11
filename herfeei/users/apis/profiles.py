9dcf1dffdf6f49258c6f6de9ce039fd6a75d9579
kouhkan <amirkouhkan1@gmail.com>
2024-01-11 21:36:46 +0330

    feat: user available avatars

--- herfeei/users/apis/profiles.py
+++ herfeei/users/apis/profiles.py
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from herfeei.api.mixins import ApiAuthMixin
-from herfeei.users.models import Profile
+from herfeei.users.models import Profile, UserAvatar
+from herfeei.users.selectors.avatars import get_user_avatars
 from herfeei.users.selectors.profile import get_profile
 
 
@@ -30,3 +31,22 @@ class ProfileView(ApiAuthMixin, APIView):
     def get(self, request):
         query = get_profile(user=request.user)
         return Response(self.OutPutSerializer(query, context={"request": request}).data)
+
+
+class GetUserAvatarsView(ApiAuthMixin, APIView):
+    class OutputUserAvatarsSerializer(serializers.ModelSerializer):
+        avatar_url = serializers.SerializerMethodField("secure_image_url")
+
+        class Meta:
+            model = UserAvatar
+            fields = ("id", "title", "slug", "avatar_url")
+
+        def secure_image_url(self, obj):
+            if not (avatar_key := obj.avatar.name):
+                return None
+            expires_in = timedelta(hours=1)
+            params = default_storage.get_object_parameters(avatar_key)
+            return default_storage.url(avatar_key, params, expire=expires_in.total_seconds())
+
+    def get(self, request):
+        return Response(self.OutputUserAvatarsSerializer(get_user_avatars(), many=True).data)

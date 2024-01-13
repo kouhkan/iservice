3e4f52430c4df7491d875629986631f3ae43ad4c
kouhkan <amirkouhkan1@gmail.com>
2024-01-13 15:32:06 +0330

    refactor: make correct config of celery

--- docker/beats_entrypoint.sh
+++ docker/beats_entrypoint.sh
@@ -4,5 +4,5 @@ echo "--> Waiting for db to be ready"
 ./wait-for-it.sh db:5432
 
 echo "--> Starting beats process"
-celery -A herfeei.tasks beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 

48c617992e7093a87a155f7e1012e8ec6a4df2eb
kouhkan <amirkouhkan1@gmail.com>
2024-01-15 14:18:53 +0330

    feat: user avatar queryset

--- herfeei/users/services/users.py
+++ herfeei/users/services/users.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 
-from herfeei.users.models import BaseUser, Profile
+from herfeei.users.models import BaseUser, Profile, UserAvatar
 
 
 def create_profile(*, user: BaseUser, ) -> Profile:
@@ -19,3 +19,7 @@ def register(*, username: str) -> BaseUser:
     if created:
         create_profile(user=user)
     return user
+
+
+def get_user_avatar_image(*, img_id: int) -> UserAvatar:
+    return UserAvatar.objects.filter(id=img_id, type=UserAvatar.AvatarType.USER).first()

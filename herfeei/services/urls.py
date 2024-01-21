3fdb526795584a2bb7c72c9a8a23c3d878dfe4d3
kouhkan <amirkouhkan1@gmail.com>
2024-01-21 23:20:06 +0330

    refactor: temp change for service urls

--- herfeei/services/urls.py
+++ herfeei/services/urls.py
@@ -5,16 +5,16 @@ from herfeei.services.apis.services import GetServiceCategoryView, GetChildrenSe
     ServiceQuestionView, UserAnswerView
 
 urlpatterns = [
-    path("answers/", UserAnswerView.as_view(), name="user-answers"),
-    path("<str:slug>/", ServiceView.as_view(), name="get-services"),
-    path("<str:slug>/questions/", ServiceQuestionView.as_view(), name="get-service-questions"),
+    path("answers", UserAnswerView.as_view(), name="user-answers"),
+    path("<str:slug>", ServiceView.as_view(), name="get-services"),
+    path("<str:slug>/questions", ServiceQuestionView.as_view(), name="get-service-questions"),
 
-    path("categories/", GetServiceCategoryView.as_view(), name="get-service-categories"),
-    path("categories/<str:slug>/", GetChildrenServiceCategoryView.as_view(), name="get-children-service-categories"),
+    path("categories", GetServiceCategoryView.as_view(), name="get-service-categories"),
+    path("categories/<str:slug>", GetChildrenServiceCategoryView.as_view(), name="get-children-service-categories"),
 
 
-    path("locations/", include([
+    path("locations", include([
         path("", GetProvincesView.as_view(), name="get-provinces"),
-        path("<str:slug>/", GetCitiesView.as_view(), name="get-cities"),
+        path("/<str:slug>", GetCitiesView.as_view(), name="get-cities"),
     ]), name="locations"),
 ]

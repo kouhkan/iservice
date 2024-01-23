88fcdbdab5aca8e535aa4a3a05e353c3983ed78c
kouhkan <amirkouhkan1@gmail.com>
2024-01-23 10:22:33 +0330

    feat: get expert queries

--- herfeei/experts/selectors/experts.py
+++ herfeei/experts/selectors/experts.py
@@ -1,8 +1,9 @@
 from typing import List
 
-from django.db.models import QuerySet, Q
+from django.db.models import QuerySet, Q, Avg, Count
 
-from herfeei.experts.models import Expert
+from herfeei.comments.models import Comment
+from herfeei.experts.models import Expert, Sample
 from herfeei.orders.models import Order
 
 
@@ -29,3 +30,37 @@ def get_available_experts(*, order: Order) -> List[QuerySet[Expert]]:
     )
 
     return suggested_experts
+
+
+def get_expert(*, expert_code: str) -> Expert:
+    return Expert.objects.filter(expert_code=expert_code).first()
+
+
+def get_expert_rate(*, expert_code: str) -> float:
+    expert_rate = Expert.objects.filter(expert_code=expert_code).annotate(
+        avg_rate=Avg('comments__rate', filter=Q(comments__status=Comment.CommentStatus.APPROVE)),
+        comment_count=Count('comments', filter=Q(comments__status=Comment.CommentStatus.APPROVE))
+    ).first()
+
+    if expert_rate:
+        return expert_rate.avg_rate or 0.0
+
+    return 0.0
+
+
+def get_expert_comments_count(*, expert_code: str) -> int:
+    return Comment.objects.filter(
+        expert__expert_code=expert_code, status=Comment.CommentStatus.APPROVE
+    ).count()
+
+
+def get_expert_comments(*, expert_code: str) -> int:
+    return Comment.objects.filter(expert__expert_code=expert_code, status=Comment.CommentStatus.APPROVE)
+
+
+def get_expert_complete_orders(*, expert_code: str) -> int:
+    return Order.objects.filter(expert__expert_code=expert_code, is_complete=True).count()
+
+
+def get_expert_samples(*, expert_code: str) -> List[QuerySet[Sample]]:
+    return Sample.objects.filter(expert__expert_code=expert_code)

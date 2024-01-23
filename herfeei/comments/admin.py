e7141b6be3025c19ef92cc3aef594979d23c4bd6
kouhkan <amirkouhkan1@gmail.com>
2024-01-23 10:53:16 +0330

    refactor: comment admin

--- herfeei/comments/admin.py
+++ herfeei/comments/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from herfeei.comments.models import Comment
 
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "expert", "description", "rate", "created_at", "status")
+    list_filter = ("status",)
+    search_fields = ("user", "expert", "description")
+    list_per_page = 50
+    raw_id_fields = ("user", "expert")
+    list_editable = ("status",)
